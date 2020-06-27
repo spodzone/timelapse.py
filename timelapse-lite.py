a9eff99716aebc8715922cb24531c02397a95bd1
Tim Haynes <github@stirfried.vegetable.org.uk>
2020-06-27 18:28:03 +0000

    Add imports

--- timelapse-lite.py
+++ timelapse-lite.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python
+
+from PIL import Image
+from sys import argv
+import os
+import glob
+import time
+from os.path import getmtime
+from PIL import Image, ImageOps, ImageChops, ImageFilter
+from PIL.Image import blend as imageBlend
