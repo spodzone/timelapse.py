c4874235c858b008844ef8c63a63a280c58c9f44
Tim Haynes <github@stirfried.vegetable.org.uk>
2020-06-27 18:28:31 +0000

    Add info logger function

--- timelapse-lite.py
+++ timelapse-lite.py
@@ -8,3 +8,7 @@ import time
 from os.path import getmtime
 from PIL import Image, ImageOps, ImageChops, ImageFilter
 from PIL.Image import blend as imageBlend
+
+def tlog(s):
+    "Output info log message S"
+    print("%s: %s" % (time.asctime(), s))
