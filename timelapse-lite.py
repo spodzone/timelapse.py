9b1c42485a67f0eb4698db1d0c52a3cf8b57ffdc
Tim Haynes <github@stirfried.vegetable.org.uk>
2020-06-27 19:00:25 +0000

    Add mktasks() and helper functions: generate list of "tasks" = tuple of two filenames and fractional proportion between by which to interpolate

--- timelapse-lite.py
+++ timelapse-lite.py
@@ -21,6 +21,35 @@ def findFiles(indir):
     filedata=sorted(zip(mtimes, fnames))
     return filedata
 
+
+def filebefore(filedata, ts):
+    "return last filename before ts"
+    return [ (fts,fname) for (fts,fname) in filedata if fts<=ts ][-1]
+
+def fileafter(filedata, ts):
+    "return first filename after ts"
+    return [ (fts,fname) for (fts,fname) in filedata if fts>=ts ][0]
+
+def tsproportion(filedata, ts):
+    "return proportion ts falls between two image timestamps"
+    leftimg=filebefore(filedata, ts)[0]
+    rightimg=fileafter(filedata, ts)[0]
+    if(leftimg==rightimg):
+        prop=0
+    else:
+        prop=(ts-leftimg)/(rightimg-leftimg)
+    return prop
+
+def mktasks(filedata, noframes):
+    "convert file data to list of interpolation tasks"
+    mints, maxts=(filedata[0][0], filedata[-1][0])
+    timestamps=[ mints+(maxts-mints)*i/noframes for i in range(noframes) ]
+    filebefores=[ filebefore(filedata, ts)[1] for ts in timestamps ]
+    fileafters=[ fileafter(filedata, ts)[1] for ts in timestamps ]
+    tasks=[ tsproportion(filedata, ts) for ts in timestamps ]
+    return list(zip(filebefores, fileafters, tasks, range(noframes)))
+
+
 def main():
     (noframes, indir, outdir) = [1500, "jpeg-in", "jpeg-out"]
     if len(sys.argv) > 1:
